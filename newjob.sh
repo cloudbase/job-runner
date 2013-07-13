adc4061a08282582555c26e197cf1dc6ecae8b0d
Alessandro Pilotti <ap@pilotti.it>
2013-07-14 01:41:28 +0300

    Added job_name to sample curl script

--- newjob.sh
+++ newjob.sh
@@ -1,2 +1,2 @@
-curl -H "Content-type: application/json" -X POST http://127.0.0.1:4000/jobs/new -d '{"job_args":["http://aaaa/bbb", "xxxxsha1xxxx"], "return_url":"http://www.cloudbase.it/testpost", "auth_key":"change_me_asap_please"}'
+curl -H "Content-type: application/json" -X POST http://127.0.0.1:4000/jobs/new -d '{"job_args":["http://aaaa/bbb", "xxxxsha1xxxx"], "return_url":"http://www.cloudbase.it/testpost", "auth_key":"your_secret_key_here", "job_name":"openstack_create_instance"}'
 
