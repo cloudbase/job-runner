b8dc2eb7344c2e9c4d134ce034d56f7404a60f89
Alessandro Pilotti <ap@pilotti.it>
2013-07-15 17:43:29 +0300

    Adds OpenStack delete instance script

---
+++ jobs/openstack_delete_instance.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+JOB_ID=$1
+VM_ID=$2
+
+source keystone_vars
+
+echo "Getting the port id for VM: $VM_ID"
+PORT_ID=`/usr/bin/quantum port-list -- --device_id $VM_ID | /bin/awk '{if (NR == 4) {print $2}}'`
+
+echo "Getting the floating ip associated to port id: $PORT_ID"
+FLOAT_IP_ID=`/usr/bin/quantum floatingip-list | /bin/awk -v port_id="$PORT_ID" '{if (NR > 3 && $8==port_id) { print $2 }}'`
+
+echo "Deleting floating ip: $FLOAT_IP_ID"
+/usr/bin/quantum floatingip-delete $FLOAT_IP_ID
+
+echo "Deleting VM: $VM_ID"
+/usr/bin/nova delete $VM_ID
+
