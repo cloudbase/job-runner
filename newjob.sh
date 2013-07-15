6249f977d3e8fb3cb7cf860b8e75db0ffc2a5cc0
Alessandro Pilotti <ap@pilotti.it>
2013-07-15 19:58:36 +0300

    Updates newjob.sh example

--- newjob.sh
+++ newjob.sh
@@ -1,2 +1,2 @@
-curl -H "Content-type: application/json" -X POST http://127.0.0.1:4000/jobs/new -d '{"job_args":["http://aaaa/bbb", "xxxxsha1xxxx"], "return_url":"http://www.cloudbase.it/testpost", "auth_key":"your_secret_key_here", "job_name":"openstack_create_instance"}'
+curl -H "Content-type: application/json" -X POST http://127.0.0.1:4000/jobs/new -d '{"job_args":["http://aaaa/bbb", "xxxxsha1xxxx"], "return_url":"http://www.cloudbase.it/testpost", "results_email":"your@email.com", "auth_key":"your_secret_key_here", "job_name":"openstack_create_instance"}'
 
